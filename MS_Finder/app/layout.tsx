eb1d0b5549ff28565bbf6b43c8039c84bfee987c
[Nikolina-B] <[nbenut00@fesb.hr]>
2026-02-09 14:54:45 +0100

    favicon postavljen

--- MS_Finder/app/layout.tsx
+++ MS_Finder/app/layout.tsx
@@ -25,6 +25,7 @@
 // </html>
 //   ); 
 // }
+
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -32,9 +33,17 @@ import Hero from "./components/Hero";
 export const metadata = {
   title: "M&S Finder",
   description: "Globalni fiksirani navbar",
-  // DODAJ OVAJ DIO ISPOD:
   icons: {
-    icon: "/favicon2.ico",
+    icon: [
+      {
+        url: "/favicon.ico", // Ikona koja će se vidjeti na Light Modeu
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        url: "/favicon2.ico.png",  // Ikona koja će se vidjeti na Dark Modeu
+        media: "(prefers-color-scheme: dark)",
+      },
+    ],
   },
 };
 
